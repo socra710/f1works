dd49dfb53d46e10fb11be13c44e3446169ed3801
김기형 <socra710@gmail.com>
2025-12-26 15:51:25 +0900

    관리자 페이지 수정

--- src/works/admin/constants.js
+++ src/works/admin/constants.js
@@ -1,5 +1,5 @@
 export const menuOptions = [
-  { key: 'expense', label: '경비 청구' },
+  { key: 'expense', label: '경비 관리' },
   { key: 'dispatch', label: '신청 관리' },
   { key: 'asset', label: '자산 관리' },
 ];
